4fd53e80beff4907df896479e2ea0965cf6a423a
Assaf Gelber <assaf.gelber@gmail.com>
2015-03-07 16:38:29 +0200

    Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  rescources :users, only: [:create, :update]
+  resources :users, only: [:create, :update]
 
-  rescources :trips, only: [:create, :index] do
+  resources :trips, only: [:create, :index] do
     put ':trip_id/stop_accepting_request', to: :stop_accepting_request
     put ':trip_id/end', to: :end_trip
   end
 
-  rescources :requests, only: [:create] do
+  resources :requests, only: [:create] do
     put ':request_id/respond', to: :respond
     put ':request_id/settle', to: :settle
   end
