93deacdc6a8c3967672db93ec4c00dbe4065cbe8
Assaf Gelber <assaf.gelber@gmail.com>
2015-03-09 15:18:04 +0200

    Remove paypal account from users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    permitted_params = params.require(:user).permit(:facebook_id, :first_name, :last_name, :phone_number, :paypal_account)
+    permitted_params = params.require(:user).permit(:facebook_id, :first_name, :last_name, :phone_number)
     permitted_params[:location_attributes] = params.require(:user).permit(:street_address, :city)
     permitted_params
   end
