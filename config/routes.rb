f194a1a28f76904857dfac9444e8a0298fce37b0
Ido Feins <ifeins@gmail.com>
2015-03-15 17:38:09 +0200

    added format json to all routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  scope '/users', controller: :users do
+  scope '/users', controller: :users, defaults: {format: :json} do
     get '/state', to: :state
     post '/login', to: :login
     post '/update', to: :update
   end
 
-  scope '/trips', controller: :trips do
+  scope '/trips', controller: :trips, defaults: {format: :json} do
     get '/', to: :index
     post '/start', to: :create
     post '/end', to: :end_trip
   end
 
-  scope '/requests', controller: :shop_requests do
+  scope '/requests', controller: :shop_requests, defaults: {format: :json} do
     get '/', to: :index
     post '/create', to: :create
     post '/cancel', to: :cancel
